ca36a8bac3df3bcd586b5eecccdc8733a2c526de
davidmelendez <Dmelz11@github.com>
2024-01-12 10:57:41 -0800

    worked on connection file clarifing connection path

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,16 @@
-const mysql require("mysql2");
+const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
     host:"localhost",
     port:"3306",
-    user: "root",
-    password: process.env.MYSQL_PASSWORD,
-    database:"employees"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-module.exports=connection;
+
+connection.connect(function (err) {
+    if (err) throw err
+})
+
+module.exports = connection;
