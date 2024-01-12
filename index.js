47b95d705fa3c78013cc437ea847b9891cd12ebf
davidmelendez <Dmelz11@github.com>
2024-01-12 10:54:35 -0800

    worked with tutor to line up server file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 const inquirer = requirer("inquirer");
+//import connection
+const db = require('./config/connection')
+
+//prompt user which action they want to take
+//array of choices: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+//use conditional statement (if or switch)
+//choice is iew all departments: execute viewAllDepartment function
+
+function viewAllDepartment() {
+    //db.query
+}
