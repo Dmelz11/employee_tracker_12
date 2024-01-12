06c962b61f6cfbf568c48370af471399c8607a98
davidmelendez <Dmelz11@github.com>
2024-01-12 11:00:56 -0800

    worked on seeds for database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,25 @@
-INSERT INTO employees (id, firstname)
+INSERT INTO department(name)
+VALUES ("Management"), 
+("Marketing"), 
+("Sales"), 
+("Accounting"),
+("Legal");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("CEO", 350000, 1),
+("Sales Manager", 90000, 2),
+("Sales Person", 65000, 3),
+("Marketing Manager",70000, 3),
+("Marketing Assistant", 50000, 2),
+("Accounting Manager", 70000, 4),
+("Accountant", 55000, 4), 
+("Chief Consule", 200000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-(001, "John"),
-(002, "Jessie"),
-(003, "Bob"),
-(004, "Susie"),
-(005, "Jamal");
+("John","Brown",1, null),
+("Jessie","James",2, 1),
+("Bob", "Brown", 3, 2),
+("Susie", "Q", 4, 1),
+("Jamal","Jordan", 5, 4);
+
