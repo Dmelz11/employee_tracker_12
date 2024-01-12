33c53b7d51331c0ad53519513ecb65ddbc738ae8
davidmelendez <Dmelz11@github.com>
2024-01-12 11:00:16 -0800

    fixed syntax error in schema file. I needed to put employee table at bottom to source the department and role table data above.

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,28 @@
---deletes any existing database with this name--
-DROP DATBASE employees_db IF EXISTS;
---creates new database called employees--
+DROP DATABASE IF EXISTS employees_db;
+
 CREATE DATABASE employees_db;
---changes to db employees for new imput route--
-USE DATABASE employees_db;
---creates table with coloumns containing numeric id and string firstname headings--
-CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
-    first_name VARCHAR (30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
-    role_id INT,
-    manager_id INT
-);
+
+USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR (30) NOT NULL
 );
 
 CREATE TABLE role (
-    id  INT PRIMARY KEY NOT NULL,
+    id  INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
