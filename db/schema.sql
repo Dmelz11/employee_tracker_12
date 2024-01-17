0c8177dc8c67e415ad861b984a3f366a64e67754
davidmelendez <Dmelz11@github.com>
2024-01-17 14:29:45 -0800

    worked on foreign keys and references

--- db/schema.sql
+++ db/schema.sql
@@ -5,24 +5,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (30) NOT NULL
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id  INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR (30),
+    id  INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR (30) NOT NULL,
-    last_name VARCHAR (30) NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY(role_id) REFERENCES department(id),
+    FOREIGN KEY(manager_id) REFERENCES department(id)
 );
