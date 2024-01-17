2aa9d9268286668076067a28825d427120ef0683
davidmelendez <Dmelz11@github.com>
2024-01-17 08:50:12 -0800

    moved a couple of lines around in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,33 @@
 INSERT INTO department(name)
-VALUES ("Management"), 
+VALUES 
+("Management"), 
 ("Marketing"), 
 ("Sales"), 
 ("Accounting"),
 ("Legal");
 
 INSERT INTO role(title, salary, department_id)
-VALUES ("CEO", 350000, 1),
+VALUES 
+("CEO", 350000, 1),
 ("Sales Manager", 90000, 2),
 ("Sales Person", 65000, 3),
-("Marketing Manager",70000, 3),
-("Marketing Assistant", 50000, 2),
-("Accounting Manager", 70000, 4),
-("Accountant", 55000, 4), 
-("Chief Consule", 200000, 5);
+("Marketing Manager",70000, 4),
+("Marketing Assistant", 50000, 5),
+("Accounting Manager", 70000, 6),
+("Accountant", 55000, 7), 
+("Chief Consule", 200000, 8);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-("John","Brown",1, null),
-("Jessie","James",2, 1),
-("Bob", "Brown", 3, 2),
-("Susie", "Q", 4, 1),
-("Jamal","Jordan", 5, 4);
+("John","Brown", 1, null),
+("Laura", "Irene", 2, 1),
+("Jessie","James", 3, 2),
+("Bob", "Jackson", 3, 2),
+("Susie", "Raincloud", 4, 1),
+("Jamal","Jordan", 5, 4),
+("Jacob", "Bornstien", 6, 1),
+("Patty", "Chin", 6, 5),
+("Besty", "Taylor", 7, 6),
+("Penny", "Many", 8, null);
+
 
