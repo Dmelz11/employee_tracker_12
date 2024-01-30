fb534a80d9166b3a92384f41d8d9936a18807f87
davidmelendez <Dmelz11@github.com>
2024-01-30 15:50:52 -0800

    added foreign keys and references to tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -21,5 +22,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_Id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
