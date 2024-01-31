bce91f0b8107092789bd66c3be5fdc0f7f9125aa
davidmelendez <Dmelz11@github.com>
2024-01-31 11:38:59 -0800

    cleaned up department ids in role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO department(department_name)
 VALUES 
-("Management"), 
+("Executive"), 
 ("Marketing"), 
 ("Sales"), 
 ("Accounting"),
@@ -9,16 +9,16 @@ VALUES
 INSERT INTO role (title, salary, department_id)
 VALUES 
 ("CEO", 350000, 1),
-("Sales Manager", 90000, 2),
+("Sales Manager", 90000, 3),
 ("Sales Person", 65000, 3),
-("Marketing Manager",70000, 4),
-("Marketing Assistant", 50000, 5),
-("Accounting Manager", 70000, 6),
-("Accountant", 55000, 7), 
-("Chief Consule", 200000, 8);
+("Marketing Manager",70000, 2),
+("Marketing Assistant", 50000, 2),
+("Accounting Manager", 70000, 4),
+("Accountant", 55000, 4), 
+("Chief Consule", 200000, 5);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id,)
 VALUES
 ("John","Brown", 1, null),
 ("Laura", "Irene", 2, 1),
@@ -27,8 +27,8 @@ VALUES
 ("Susie", "Raincloud", 4, 1),
 ("Jamal","Jordan", 5, 4),
 ("Jacob", "Bornstien", 6, 1),
-("Patty", "Chin", 6, 5),
+("Patty", "Chin", 7, 6),
 ("Besty", "Taylor", 7, 6),
-("Penny", "Many", 8, null);
+("Penny", "Many", 8, 1);
 
 
